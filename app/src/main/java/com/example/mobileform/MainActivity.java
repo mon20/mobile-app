994745f06ef4ca9c8aa0b2c8ebe8a9f90c3b16b2
movelar93 <movelar93@gmail.com>
2021-01-30 20:36:16 -0300

    add date picker

--- app/src/main/java/com/example/mobileform/MainActivity.java
+++ app/src/main/java/com/example/mobileform/MainActivity.java
@@ -9,12 +9,20 @@ import android.widget.Button;
 import com.google.android.material.datepicker.CalendarConstraints;
 import com.google.android.material.datepicker.MaterialCalendar;
 import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import com.google.android.material.textfield.TextInputEditText;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
     private Button mDatePickerBtn;
+
+    private TextInputEditText selectedDate;
+
+    private String fecha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
         calendar.setTimeInMillis(today);
 
-        calendar.set(Calendar.MONTH, Calendar.JANUARY);
-    long january = calendar.getTimeInMillis();
 
-        calendar.set(Calendar.MONTH, Calendar.MARCH);
-    long march = calendar.getTimeInMillis();
-
-        calendar.set(Calendar.MONTH, Calendar.DECEMBER);
-    long december = calendar.getTimeInMillis();
 
     //CalendarConstraints
    // CalendarConstraints.Builder constraintBuilder = new CalendarConstraints.Builder();
@@ -57,5 +58,16 @@ public class MainActivity extends AppCompatActivity {
             materialDatePicker.show(getSupportFragmentManager(), "DATE_PICKER");
         }
     });
+
+        materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener <Long>() {
+            @Override
+            public void onPositiveButtonClick(Long selection) {
+                Calendar utc = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+                utc.setTimeInMillis(selection);
+                SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+                String formatted = format.format(utc.getTime());
+                selectedDate.setText(formatted);
+            }
+        });
     }
 }
