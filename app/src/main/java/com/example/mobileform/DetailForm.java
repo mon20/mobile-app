652d7f7c6922084e94122a239e5ffdc86f5a915c
movelar93 <movelar93@gmail.com>
2021-02-05 20:23:49 -0300

    add style and colors

--- app/src/main/java/com/example/mobileform/DetailForm.java
+++ app/src/main/java/com/example/mobileform/DetailForm.java
@@ -2,10 +2,14 @@ package com.example.mobileform;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class DetailForm extends AppCompatActivity {
+    Button btnEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +20,7 @@ public class DetailForm extends AppCompatActivity {
         String nombre = parameters.getString(getResources().getString(R.string.paramName));
         String date = parameters.getString(getResources().getString(R.string.paramDate));
         String email = parameters.getString(getResources().getString(R.string.paramDate));
-        String phone =   parameters.getString(getResources().getString(R.string.paramEmail));
+        String phone = parameters.getString(getResources().getString(R.string.paramEmail));
         String descripcion = parameters.getString(getResources().getString(R.string.paramDescription));
 
         TextView tvNombre = (TextView) findViewById(R.id.textViewName);
@@ -30,5 +34,20 @@ public class DetailForm extends AppCompatActivity {
         tvEmail.setText(email);
         tvPhone.setText(phone);
         tvDescription.setText(descripcion);
+
+        btnEdit.findViewById(R.id.edit_btn);
+        Intent intent = new Intent(this, MainActivity.class);
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+            /*intent.putExtra(getResources().getString(R.string.paramName), nombre.getText().toString() );
+            intent.putExtra(getResources().getString(R.string.paramDate), selectedDate.getText().toString() );
+            intent.putExtra(getResources().getString(R.string.paramDate), email.getText().toString() );
+            intent.putExtra(getResources().getString(R.string.paramPhone), telefono.getText().toString() );
+            intent.putExtra(getResources().getString(R.string.paramDescription), descripcion.getText().toString() );*/
+                startActivity(intent);
+            }
+        });
     }
 }
